67d3baeb133e8a6dfd5e91e56ee443c5f3f458b0
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-10-26 16:33:43 -0300

    exercicio 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 
 class Form extends Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick
+    } = this.props;
     return (
       <form>
         <label htmlFor="name">
           Nome:
-          <input type="text" data-testid="name-input" id="name" />
+          <input type="text" data-testid="name-input" id="name" value={ cardName } />
         </label>
         <label htmlFor="textarea">
           Description:
@@ -14,35 +28,60 @@ class Form extends Component {
             type="textarea"
             id="textarea"
             data-testid="description-input"
+            value={ cardDescription }
           />
         </label>
         <label htmlFor="atrr1">
           atribute 1:
-          <input type="number" data-testid="attr1-input" />
+          <input type="number" data-testid="attr1-input" value={ cardAttr1 } />
         </label>
         <label htmlFor="atrr2">
           Atribute 2:
-          <input type="number" data-testid="attr2-input" />
+          <input type="number" data-testid="attr2-input" value={ cardAttr2 } />
         </label>
         <label htmlFor="atrr3">
           Atribute 3:
-          <input type="number" data-testid="attr3-input" />
+          <input type="number" data-testid="attr3-input" value={ cardAttr3 } />
         </label>
         <label htmlFor="image">
           Image:
-          <input type="text" data-testid="image-input" id="image" />
+          <input
+            type="text"
+            data-testid="image-input"
+            id="image"
+            value={ cardImage }
+            onChange={ onInputChange() }
+          />
         </label>
         <label htmlFor="rareSelect">
-          <select data-testid="rare-input" id="rareSelect">
+          <select
+            data-testid="rare-input"
+            id="rareSelect"
+            value={ cardRare }
+            onChange={ onInputChange }
+          >
             <option value="normal">normal</option>
             <option value="raro">raro</option>
             <option value="muito raro">muito raro</option>
           </select>
         </label>
         <label htmlFor="trunfo">
-          <input type="checkbox" id="trunfo" data-testid="trunfo-input" />
+          <input
+            type="checkbox"
+            id="trunfo"
+            data-testid="trunfo-input"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
+          />
         </label>
-        <button type="submit" data-testid="save-button">Salvar</button>
+        <button
+          type="submit"
+          data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+        >
+          Salvar
+        </button>
       </form>
     );
   }
